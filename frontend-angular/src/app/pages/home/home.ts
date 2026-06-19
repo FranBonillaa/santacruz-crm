732df53be73d10442eef5c6b91e9286fc9100a8d
FranBonilla88 <fbonillavaro@gmail.com>
2026-06-19 10:33:07 +0200

    feat: add hero component with Tailwind CSS styles

--- frontend-angular/src/app/pages/home/home.ts
+++ frontend-angular/src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar';
+import { HeroComponent } from '../../components/hero/hero';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, HeroComponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
