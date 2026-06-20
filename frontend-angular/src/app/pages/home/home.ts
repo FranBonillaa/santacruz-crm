e93cd728bc4f3404e233898ff238dcb12c5b0ab0
FranBonilla88 <fbonillavaro@gmail.com>
2026-06-20 18:22:57 +0200

    add packages section with header

--- frontend-angular/src/app/pages/home/home.ts
+++ frontend-angular/src/app/pages/home/home.ts
@@ -3,10 +3,17 @@ import { NavbarComponent } from '../../components/navbar/navbar';
 import { HeroComponent } from '../../components/hero/hero';
 import { ServicesComponent } from '../../components/services/services';
 import { FeaturesComponent } from '../../components/features/features';
+import { PackagesComponent } from '../../components/packages/packages';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, HeroComponent, ServicesComponent, FeaturesComponent],
+  imports: [
+    NavbarComponent,
+    HeroComponent,
+    ServicesComponent,
+    FeaturesComponent,
+    PackagesComponent,
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
