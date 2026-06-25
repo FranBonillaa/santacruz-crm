11f3e088b1b14eb0d1f30f699e2859fef7aa8457
FranBonilla88 <fbonillavaro@gmail.com>
2026-06-25 14:55:15 +0200

    feat: add privacy polity page

--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -1,4 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
+import { PrivacyComponent } from './pages/privacy/privacy';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'privacy',
+    component: PrivacyComponent,
+  },
+];
