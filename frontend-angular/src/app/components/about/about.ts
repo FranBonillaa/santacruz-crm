a09fe91a3e89da50b82d294729dfe20d4278e7c2
FranBonilla88 <fbonillavaro@gmail.com>
2026-06-26 17:10:15 +0200

    feat: add about section styles and Swiper carousel for desktop

--- frontend-angular/src/app/components/about/about.ts
+++ frontend-angular/src/app/components/about/about.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'app-about',
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
