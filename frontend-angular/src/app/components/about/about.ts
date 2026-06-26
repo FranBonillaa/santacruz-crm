377cc28e83f5d2a5b3345e29a8aaa32199d555d7
FranBonilla88 <fbonillavaro@gmail.com>
2026-06-26 18:06:14 +0200

    feat: add GSAP scroll animations to about section

--- frontend-angular/src/app/components/about/about.ts
+++ frontend-angular/src/app/components/about/about.ts
@@ -1,5 +1,9 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 import { register } from 'swiper/element/bundle';
+
+gsap.registerPlugin(ScrollTrigger);
 
 register();
 
@@ -10,4 +14,18 @@ register();
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
-export class AboutComponent {}
+export class AboutComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    gsap.from('.about-content > *', {
+      opacity: 0,
+      y: 30,
+      duration: 0.8,
+      stagger: 0.2,
+      ease: 'power2.out',
+      scrollTrigger: {
+        trigger: '.about-content',
+        start: 'top 80%',
+      },
+    });
+  }
+}
